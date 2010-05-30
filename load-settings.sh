65e80272a78bafe45a7aa33dea858334b4a645ab
Aaron Faanes <dafrito@gmail.com>
2010-05-30 15:09:18 -0500

    Removed load-settings

--- load-settings.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-[ -d "$BACKUP_EXECUTABLE_DIR" ] || BACKUP_EXECUTABLE_DIR=${0%/*}
-source $BACKUP_EXECUTABLE_DIR/library.sh || exit 1
-
-ROOT=${ROOT-$HOME/.backup}
-mkdir -p "$ROOT/config" || die "$ROOT could not be created"
-
-TARGETS=${TARGETS-$ROOT/config/targets}
-[ -e $TARGETS ] || touch $TARGETS
