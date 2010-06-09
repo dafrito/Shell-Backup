853ec7214d29f6406885a018b7ab465329086db9
Aaron Faanes <dafrito@gmail.com>
2010-06-09 15:06:31 -0500

    Rearranged layout once again

--- library.sh
+++ library.sh
@@ -1,4 +1,39 @@
 #!/bin/bash
+if [ ! -d "$BACKUP_EXECUTABLE_DIR" ]; then
+	echo "BACKUP_EXECUTABLE_DIR must be defined"
+	exit 1
+fi
+PATH=/bin:/usr/bin:$BACKUP_EXECUTABLE_DIR
+source "load-settings.sh" || exit 1
+
+if [ "$DRY_RUN" ]; then
+	dry="--dry-run";
+fi
+
+FAILURES=0
+function error {
+	let FAILURES++
+	echo $* 1>&2
+}
+
+function push {
+	git push $dry $* 2>&1 || error "Failed to push"
+}
+
+function sync {
+	local OS_SPECIFIC="-pgol"
+	if [ "$WINDOWS" ]; then
+		OS_SPECIFIC="-L"
+	fi
+	rsync $verbose $dry -ih -rtzR $OS_SPECIFIC --exclude-from ~/.gitignore --exclude '.git/***' $* $path || 
+		error "Failed to sync"
+}
+
+function all_targets {
+	cat $TARGETS | sed -e 's/[\t ].*$//'
+}
+
+trap 'exit $FAILURES' EXIT
 
 function log {
 	[ "$VERBOSE" ] && echo $*
